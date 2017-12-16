f8670ac22595824541c919e9a2b190d2cc00678f
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-16 00:10:01 -0800

    document.body cleans up needless var calling

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let streak = 0;
+
 function init() {
-  const body = document.querySelector('body');
-  body.addEventListener('keydown', function(e) {
+  let streak = 0;
+
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[streak]) {
       streak++;
       console.log(`the streak is at ${streak}`);
+
       if (streak === code.length) {
         alert("Snake?? SNAAAAAAKEEE!!!");
+        streak = 0;
       }
     } else {
       streak = 0;
       console.log(`the streak is back to ${streak}`);
     }
-  })
+  });
 }
 
 
