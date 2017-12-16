52941aa661dd5f95a70091b2bd846cb4dff7d90d
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-16 00:06:22 -0800

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let streak = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[streak]) {
+      streak++;
+      console.log(`the streak is at ${streak}`);
+      if (streak === code.length) {
+        alert("Snake?? SNAAAAAAKEEE!!!");
+      }
+    } else {
+      streak = 0;
+      console.log(`the streak is back to ${streak}`);
+    }
+  })
 }
+
+
+
+// typing things in body will log some stuff.
